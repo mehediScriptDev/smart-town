7379cd1487b939b437076e11f591e93cd81f5e64
Mehedi Mehedi <mehedi25122003@gmail.com>
2025-05-08 11:45:28 +0600

    error p0age added

---
+++ src/Components/Error.jsx
@@ -0,0 +1,11 @@
+import errorimg from '../images/404.gif'
+
+const Error = () => {
+    return (
+        <section>
+            <img src={errorimg} alt="" />
+        </section>
+    );
+};
+
+export default Error;
