4d27d33c98300f7694e29abf53e118a399ff1530
Mehedi Mehedi <mehedi25122003@gmail.com>
2025-05-08 15:49:30 +0600

    err page done

--- src/Components/Error.jsx
+++ src/Components/Error.jsx
@@ -1,9 +1,14 @@
+import { Link } from 'react-router-dom';
 import errorimg from '../images/404.gif'
+import { IoMdArrowRoundBack } from "react-icons/io";
 
 const Error = () => {
     return (
-        <section className='flex justify-center items-center'>
+        <section className='w-10/12 mx-auto py-3'>
+            <Link to={'/'} className='text-2xl md:text-3xl font-semibold flex items-center'><IoMdArrowRoundBack /> Back Home</Link>
+            <section className='flex justify-center items-center'>
             <img src={errorimg} alt="" />
+        </section>
         </section>
     );
 };
