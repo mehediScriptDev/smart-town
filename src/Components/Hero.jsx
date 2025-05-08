eba62dc831507ed32ad03cf352397894f074ba5a
Mehedi Mehedi <mehedi25122003@gmail.com>
2025-05-09 00:15:59 +0600

    all done hero

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -3,13 +3,13 @@ import "react-responsive-carousel/lib/styles/carousel.min.css";
 import { Link } from "react-router-dom";
 
 const images = [
-
-  "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/0e/94/5b/61/mohera-jomidar-bari.jpg?w=1400&h=1400&s=1",
-  "https://cf.bstatic.com/xdata/images/hotel/max1024x768/499218110.jpg?k=8c01cf92f0d36c54eb07d56695faabb3181e506be231e5d1a113c6f2bac5217d&o=&hp=1",
-  "https://images.hive.blog/DQmcwmx3hYm544ykjzoziveDzPPj5Rreqgu47CfewHk6d3g/IMG_1679300179138.jpg",
+  "https://admin.watergardenresort.com/media/65841b43817fc.jpg",
+  "https://adarbepari.com/wp-content/uploads/2016/07/mohera-jamidar-bari-tangail-02.jpg",
+  
+  "https://as1.ftcdn.net/v2/jpg/05/42/26/14/1000_F_542261433_xRur6d1R6e7H4fQUNEzHIKHvr5ikN2gE.jpg",
   "https://bdsearcher.com/wp-content/uploads/2017/12/Nagarpur-Zamindar-Bari.jpg",
   "https://lh3.googleusercontent.com/p/AF1QipOkuR4jS2Ae1n03NHlasD9J6MCP6NkRtfPrZYgS=w540-h312-n-k-no",
-  "https://encrypted-tbn2.gstatic.com/licensed-image?q=tbn:ANd9GcQKCtUgN6B4dPqy1vPwa6-T6jRENZCc_GiAx-Lj0sF_EDPtDjrqwzLFWyntoMtwowUxWz04SeYADrG6zkNjXaZp_hR_htOAejz1T_cyorM",
+  "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/11/87/87/5b/mosque-of-korotia-zamidar.jpg?w=1200&h=-1&s=1",
   "https://lh3.googleusercontent.com/gps-cs-s/AC9h4noPoFXYT_i-6Ix7jl0cwPEb6SygwDtIPBcD_O-HhJ1zlWw-FuI8hhawhwDOT2yBMa6oYgCxPHgpQSKfciP7BS_-1poWxEWRBL4Y0s_w4OofoKjW1SmnXQ8IYawYK5UUxqIMDlC8=w540-h312-n-k-no",
   "https://lh3.googleusercontent.com/gps-cs-s/AC9h4nonN5j-j4cgU4bZs-oZkuJ2dKFXxIQXVcji4EBCCt3DrtFQ-9Izeyfuu3PG2ApCgGwg-Q88BmosUVQZyGzPVOYLgYnjolQm0v5hnX5yAX6a_hG5VfeY7l1vSvzrh_0lQ2Z8Wz0=w540-h312-n-k-no",
   "https://encrypted-tbn2.gstatic.com/licensed-image?q=tbn:ANd9GcQnR4g2_rKfJurbwChJVWphfbF4eApXRn1W2vkK2uby80kjR61nzgvGWA_KWDReAGAtcxs6Eim8xeujUxHGgjvcN4TMuFg5f7WYCXwGkCQ",
@@ -42,7 +42,7 @@ const Hero = () => {
           <Carousel useKeyboardArrows={true}>
             {images.map((URL, index) => (
               <div className="slide border-mainbg border-2">
-                <img className="min-h-8" alt="sample_file" src={URL} key={index} />
+                <img className="" alt="sample_file" src={URL} key={index} />
               </div>
             ))}
           </Carousel>
