0c6a49861da1f060b5bf11b26b14be38c6d3012e
Mehedi Mehedi <mehedi25122003@gmail.com>
2025-05-08 16:12:46 +0600

    nav logo and fav added

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -1,5 +1,6 @@
 import { IoMenu } from "react-icons/io5";
 import { NavLink } from "react-router-dom";
+import logo from '../../public/logo.png'
 const Nav = () => {
   const links = (
     <>
@@ -38,14 +39,19 @@ const Nav = () => {
                 {links}
               </ul>
             </div>
-            <a className="join text-2xl md:text-3xl"><span className="font-alfa font-semibold join-item">Smart</span><span className="join-item font-italy text-btncl">Town</span></a>
+            <div className="hidden md:block">
+             <img className="w-10" src={logo} alt="logo" />
+             </div>
+            <a className="join text-2xl md:text-3xl flex items-center"><span className="font-alfa font-semibold join-item">
+             
+              Smart</span><span className="join-item font-italy text-btncl">Town</span></a>
           </div>
           <div className=" hidden lg:flex">
             <ul className="menu menu-horizontal px-1">{links}</ul>
           </div>
           </div>
           <div className="flex gap-2 items-center">
-          <a className="btn btn-ghost hover:bg-mainbg hover:text-white hover:border-transparent hidden md:flex">Button</a>
+          <a className="btn btn-ghost hover:bg-mainbg hover:text-white hover:border-transparent hidden md:flex">Emergency</a>
             <a className="btn btn-sm md:btn-md btn-outline rounded-full hover:text-mainbg hover:bg-btncl hover:border-transparent">Featured Places</a>
             
           </div>
