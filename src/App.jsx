c23ea55b989ac18757ada2b747c81a741723a3e6
Mehedi Mehedi <mehedi25122003@gmail.com>
2025-05-08 16:38:27 +0600

    footer dnoe

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 
+import { Outlet } from 'react-router-dom'
 import './App.css'
 import Nav from './Components/Nav'
+import Footer from './Components/Footer'
 
 function App() {
 
   return (
     <>
       <Nav></Nav>
+      <Outlet></Outlet>
+      <Footer></Footer>
     </>
   )
 }
