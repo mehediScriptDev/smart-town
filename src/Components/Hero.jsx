8de42b74be958442873243e2c265d84f3365fd72
Mehedi Mehedi <mehedi25122003@gmail.com>
2025-05-08 23:20:11 +0600

    hero done for now

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -1,10 +1,15 @@
 import { Carousel } from "react-responsive-carousel";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Link } from "react-router-dom";
 
 const images = [
+  "https://lh3.googleusercontent.com/p/AF1QipOkuR4jS2Ae1n03NHlasD9J6MCP6NkRtfPrZYgS=w540-h312-n-k-no",
   "https://encrypted-tbn2.gstatic.com/licensed-image?q=tbn:ANd9GcQKCtUgN6B4dPqy1vPwa6-T6jRENZCc_GiAx-Lj0sF_EDPtDjrqwzLFWyntoMtwowUxWz04SeYADrG6zkNjXaZp_hR_htOAejz1T_cyorM",
   "https://lh3.googleusercontent.com/gps-cs-s/AC9h4noPoFXYT_i-6Ix7jl0cwPEb6SygwDtIPBcD_O-HhJ1zlWw-FuI8hhawhwDOT2yBMa6oYgCxPHgpQSKfciP7BS_-1poWxEWRBL4Y0s_w4OofoKjW1SmnXQ8IYawYK5UUxqIMDlC8=w540-h312-n-k-no",
   "https://lh3.googleusercontent.com/gps-cs-s/AC9h4nonN5j-j4cgU4bZs-oZkuJ2dKFXxIQXVcji4EBCCt3DrtFQ-9Izeyfuu3PG2ApCgGwg-Q88BmosUVQZyGzPVOYLgYnjolQm0v5hnX5yAX6a_hG5VfeY7l1vSvzrh_0lQ2Z8Wz0=w540-h312-n-k-no",
+  "https://encrypted-tbn2.gstatic.com/licensed-image?q=tbn:ANd9GcQnR4g2_rKfJurbwChJVWphfbF4eApXRn1W2vkK2uby80kjR61nzgvGWA_KWDReAGAtcxs6Eim8xeujUxHGgjvcN4TMuFg5f7WYCXwGkCQ",
+  "https://lh3.googleusercontent.com/p/AF1QipMASgKcZtws98B5sAF-u4lpH9YpzbSgrlyKpLYP=w540-h312-n-k-no",
+  "https://encrypted-tbn1.gstatic.com/licensed-image?q=tbn:ANd9GcT9AJL7ZAYo2usnGUicMgtdaopb88yi06cm-DLSDDYNFQN8EirbI8JSMOiNi5Xw-9xdgfLw_mCDG8Bgzvx1Br63wIZP19bl7rZzuNxyu-U",
 ];
 
 const Hero = () => {
@@ -24,17 +29,19 @@ const Hero = () => {
             hospitality. 
           </p>
           <div className="pb-5">
-            <button className="btn bg-btncl text-mainbg rounded-full hover:bg-white duration-100">Explore Tangail</button>
+            <a href="#explore" className="btn bg-btncl text-mainbg rounded-full hover:bg-white duration-100">Explore Tangail</a>
           </div>
         </div>
-        <div className="box col-span-6">
+        <div id="explore" className="box col-span-6 bg-mainbg">
+          <div className="md:w-10/12 mx-auto md:border-4 md:border-mainbg">
           <Carousel useKeyboardArrows={true}>
             {images.map((URL, index) => (
               <div className="slide border-mainbg border-2">
-                <img className="" alt="sample_file" src={URL} key={index} />
+                <img className="min-h-min" alt="sample_file" src={URL} key={index} />
               </div>
             ))}
           </Carousel>
+          </div>
         </div>
       </div>
     </section>
