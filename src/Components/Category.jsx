1d78676615e93fc1d9eb4b6a701a8a189cbbb4cd
Mehedi Mehedi <mehedi25122003@gmail.com>
2025-05-09 07:42:10 +0600

    category section almost done

--- src/Components/Category.jsx
+++ src/Components/Category.jsx
@@ -4,11 +4,15 @@ import { BsBank } from "react-icons/bs";
 import { IoIosSearch } from "react-icons/io";
 import { LiaHotelSolid } from "react-icons/lia";
 import { GiShop } from "react-icons/gi";
+import { IoRestaurantSharp } from "react-icons/io5";
+import { BsBusFront } from "react-icons/bs";
+import { FaMapLocationDot } from "react-icons/fa6";
+import { MdOutlineLocalGroceryStore } from "react-icons/md";
 
 const Category = () => {
   return (
     <section className="min-h-screen ">
-      <div className="mt-8">
+      <div className="mt-8 w-11/12 mx-auto">
         <h1 className="font-plus text-center font-bold md:pb-8 text-3xl md:text-4xl text-mainbg py-3">
           Discover Everything About Our Town
         </h1>
@@ -53,6 +57,36 @@ const Category = () => {
               
             </p>
             <h3 className="md:text-xl text-xs font-semibold">Super Shops</h3>
+          </Link>
+
+          <Link className="bg-mainbg hover:bg-[#003b7a] transition-colors duration-300 text-white rounded-2xl shadow-lg border-t-4 border-btncl p-4 md:p-6 flex flex-col items-center justify-center space-y-4 cursor-pointer hover:scale-105">
+            <p>
+              <IoRestaurantSharp className="md:text-4xl text-2xl text-btncl" />
+              
+            </p>
+            <h3 className="md:text-xl text-xs font-semibold">Restaurants</h3>
+          </Link>
+
+          <Link className="bg-mainbg hover:bg-[#003b7a] transition-colors duration-300 text-white rounded-2xl shadow-lg border-t-4 border-btncl p-4 md:p-6 flex flex-col items-center justify-center space-y-4 cursor-pointer hover:scale-105">
+            <p>
+              <MdOutlineLocalGroceryStore className="md:text-4xl text-2xl text-btncl" />
+              
+            </p>
+            <h3 className="md:text-xl text-xs font-semibold">Local Markets</h3>
+          </Link>
+          <Link className="bg-mainbg hover:bg-[#003b7a] transition-colors duration-300 text-white rounded-2xl shadow-lg border-t-4 border-btncl p-4 md:p-6 flex flex-col items-center justify-center space-y-4 cursor-pointer hover:scale-105">
+            <p>
+              <FaMapLocationDot className="md:text-4xl text-2xl text-btncl" />
+              
+            </p>
+            <h3 className="md:text-xl text-xs font-semibold">Tourist Attractions</h3>
+          </Link>
+          <Link className="bg-mainbg hover:bg-[#003b7a] transition-colors duration-300 text-white rounded-2xl shadow-lg border-t-4 border-btncl p-4 md:p-6 flex flex-col items-center justify-center space-y-4 cursor-pointer hover:scale-105">
+            <p>
+              <BsBusFront className="md:text-4xl text-2xl text-btncl" />
+              
+            </p>
+            <h3 className="md:text-xl text-xs font-semibold">Transportation</h3>
           </Link>
         </div>
       </div>
