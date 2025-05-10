16e34ad40774ac5785e22535382217add7dc0285
Mehedi Mehedi <mehedi25122003@gmail.com>
2025-05-10 23:22:30 +0600

    hero section edited

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -1,6 +1,5 @@
 import { Carousel } from "react-responsive-carousel";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
-import { Link } from "react-router-dom";
 import Category from "./Category";
 import Feature from "./Feature";
 import Emergency from "./Emergency";
